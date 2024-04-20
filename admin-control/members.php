3d3eeb47f0f3879da4b2e9d1dffde71f76e041d5
amira7190 <amira7190@github.com>
2024-04-20 03:54:57 +0200

    add member into insert

--- admin-control/members.php
+++ admin-control/members.php
@@ -13,9 +13,71 @@ $pageTitle = 'Members';
             include 'init.php';
 
             $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
-            if($do == 'Manage'){
-                //manage page
-            } elseif ($do == 'Edit'){//Edit Page
+            if($do == 'Manage'){ //manage page
+                 
+                          echo "welcome to manage member ";
+                          echo "</br>";
+                          echo' <a href = "members.php?do=Add"> add new member</a>';
+            } elseif ($do == 'Add'){ ?>   <!--Add Members Page-->
+                        
+              <h1 class = "text-center  "> Add New Member</h1>
+              <div class="container">
+                    <form class="form-horizontal" action ="?do=Insert" method ="POST">
+                                     <!-- Start username filed -->
+                                <div class="form-group form-group-lg">
+                                    <label class = "col-sm-2 control-label"> Username</label>
+                                     <div class="col-sm-10">
+                                         <input type="text" name="username" class="form-control" autocomplete="off"  required = "required"/>
+                                      </div>
+                                 </div>
+                                      <!-- end username filed -->
+
+
+                                     <!-- Start password filed -->
+                                 <div class="form-group form-group-lg">
+                                     <label class = "col-sm-2 control-label"> Password</label>
+                                     <div class="col-sm-10">
+                                          <input type="password" name="password" class="form-control"  autocomplete="new-password" placeholder = ""/>
+                                     </div>
+                                  </div>
+                                        <!-- end password filed -->
+
+
+                                        <!-- Start email filed -->
+                                  <div class="form-group form-group-lg">
+                                      <label class = "col-sm-2 control-label"> Email</label>
+                                      <div class="col-sm-10">
+                                         <input type="email" name="email"  class="form-control"  required = "required" placeholder = ""/>
+                                      </div>
+                                  </div>
+                                       <!-- end email filed -->
+
+
+                                        <!-- Start fullname filed -->
+                                  <div class="form-group form-group-lg">
+                                      <label class = "col-sm-2 control-label"> Full Name</label>
+                                      <div class="col-sm-10">
+                                             <input type="text" name="full"  class="form-control"  required = "required" placeholder =""/>
+                                      </div>
+                                  </div>
+                                        <!-- end fullname filed -->
+
+
+                                        <!-- Start submit filed -->
+                                   <div class="form-group form-group-lg">
+                                        <div class=" col-sm-offset-2 col-sm-10">
+                                               <input type="submit" value="Add Member" class="btn btn-primary btn-lg" />
+                                         </div>
+                                   </div>
+                                        <!-- end submit filed -->
+                 </div>
+
+              </form>
+
+        </div> 
+                        
+
+      <?php      } elseif ($do == 'Edit'){//Edit Page
               // echo 'welcome to edit your id is' . $_GET['userid'];
                        
                          //check if Get Request Userid Is Numeric & Get The Integer Value Of It
