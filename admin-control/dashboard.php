06e4e837f8a66be5f028dfc74e7809b20008de0f
amira7190 <amira7190@github.com>
2024-04-30 22:51:36 +0300

    dashboard design layout

--- admin-control/dashboard.php
+++ admin-control/dashboard.php
@@ -7,6 +7,75 @@
           if (isset($_SESSION['Username'])){
             $pageTitle = 'Dashboard'; 
                      include 'init.php';
+                     /*Start Dashboard Page */
+                     ?>
+                     <div class="container home-stats text-center">
+                        <h1>Dashboard</h1>
+                        <div class="row">
+                              <div class="col-md-3">
+                                    <div class="stat border ">
+                                         Total Members
+                                         <span class="d-block fs-1">200</span>
+                                    </div>
+                              </div>
+                              <div class="col-md-3">
+                                    <div class="stat border">
+                                          Pending Members
+                                          <span class="d-block fs-1">25</span>
+                                    </div>
+                              </div>
+                              <div class="col-md-3">
+                                    <div class="stat border">
+                                          Total Items
+                                          <span class="d-block fs-1">1500</span>
+                                    </div>
+                              </div>
+                              <div class="col-md-3">
+                                    <div class="stat border">
+                                          Total Comments
+                                          <span class="d-block fs-1">3500</span>
+                                    </div>
+                              </div>
+
+                        </div>
+                     </div>
+                     <div class="container latest">
+                        <div class="row">
+                              <div class="col-sm-6">
+                                    <div class="panel panel-default">
+                                          <div class="panel-heading">
+                                                <i class="fa fa-users">Latest Registerd Users</i>
+                                          </div>
+                                          <div class="panel-body border ">
+                                                Test
+                                          </div>
+                                    </div>
+                              </div>
+                              <div class="col-sm-6">
+                                    <div class="panel panel-default">
+                                          <div class="panel-heading">
+                                                <i class="fa fa-tag">Latest Items</i>
+                                          </div>
+                                          <div class="panel-body border ">
+                                                Test
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
+                     </div>
+
+
+
+
+
+
+
+
+
+
+
+                    <?php
+                     /*End Dashboard Page*/
                      
                      include $tpl . 'footer.php'; 
 
